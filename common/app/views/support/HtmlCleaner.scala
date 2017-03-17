dc2d4e0a1af95018a13f793fca27dd9d07039227
blongden73 <hello@benlongdendesign.co.uk>
2017-03-17 15:08:14 +0000

    removed drop caps from recipes

--- common/app/views/support/HtmlCleaner.scala
+++ common/app/views/support/HtmlCleaner.scala
@@ -532,7 +532,7 @@ case class ImmersiveHeaders(isImmersive: Boolean) extends HtmlCleaner {
   }
 }
 
-case class DropCaps(isFeature: Boolean, isImmersive: Boolean) extends HtmlCleaner {
+case class DropCaps(isFeature: Boolean, isImmersive: Boolean, isRecipeArticle: Boolean) extends HtmlCleaner {
   private def setDropCap(p: Element): String = {
     p.html.replaceFirst(
       "^([\"'“‘]*[a-zA-Z])(.{199,})",
@@ -541,7 +541,7 @@ case class DropCaps(isFeature: Boolean, isImmersive: Boolean) extends HtmlCleane
   }
 
   override def clean(document: Document): Document = {
-    if(isFeature) {
+    if(isFeature && !isRecipeArticle) {
       val children = document.body().children().toList
       children.headOption match {
         case Some(p) =>
